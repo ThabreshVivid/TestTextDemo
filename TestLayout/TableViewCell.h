3ddbb92f528450a7e7f5ae4934044235d3443666
ThabreshVivid <subramanibr@thabresh.local>
2016-09-01 15:11:33 +0530

    Initial Commit

---
+++ TestLayout/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  TestLayout
+//
+//  Created by Thabresh on 9/1/16.
+//  Copyright © 2016 VividInfotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
---
+++ TestLayout/ViewController.m
@@ -0,0 +1,43 @@
+//
+//  ViewController.m
+//  TestLayout
+//
+//  Created by Thabresh on 9/1/16.
+//  Copyright © 2016 VividInfotech. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "TableViewCell.h"
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *txtTbl;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.txtTbl registerNib:[UINib nibWithNibName:@"TableViewCell" bundle:nil] forCellReuseIdentifier:@"TableViewCell"];
+    self.txtTbl.estimatedRowHeight = 51;
+    self.txtTbl.rowHeight = UITableViewAutomaticDimension;
+    // Do any additional setup after loading the view, typically from a nib.
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    TableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TableViewCell"];
+    return cell;
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return  UITableViewAutomaticDimension;
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
